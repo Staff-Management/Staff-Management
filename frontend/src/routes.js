979dd1e7ec16a199f6eb86714c69df68a8614fe6
Erika Yi <yi.erika2@gmail.com>
2022-01-06 11:04:50 -0500

    deleted unnecessary page - notification

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -38,7 +38,6 @@ Coded by www.creative-tim.com
 // Material Dashboard 2 React layouts
 import Dashboard from "layouts/dashboard";
 import Tables from "layouts/tables";
-import Notifications from "layouts/notifications";
 
 // @mui icons
 import Icon from "@mui/material/Icon";
@@ -59,14 +58,6 @@ const routes = [
     icon: <Icon fontSize="small">table_view</Icon>,
     route: "/tables",
     component: <Tables />,
-  },
-  {
-    type: "collapse",
-    name: "Notifications",
-    key: "notifications",
-    icon: <Icon fontSize="small">notifications</Icon>,
-    route: "/notifications",
-    component: <Notifications />,
   },
 ];
 
