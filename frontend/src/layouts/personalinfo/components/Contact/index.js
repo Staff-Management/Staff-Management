869816636fa1a3caa1ad37ccfef0f21c22c8c729
Erika Yi <yi.erika2@gmail.com>
2022-01-07 17:17:29 -0500

    add styling for personal dashboard

--- frontend/src/layouts/personalinfo/components/Contact/index.js
+++ frontend/src/layouts/personalinfo/components/Contact/index.js
@@ -1,5 +1,6 @@
 // @mui material components
 import Grid from "@mui/material/Grid";
+import Typography from '@mui/material/Typography';
 
 // Material Dashboard 2 React components
 import MDBox from "components/MDBox";
@@ -18,23 +19,26 @@ function Contact() {
             <Grid container spacing={3}>
                 <Grid item xs={12} md={12} lg={12}>
                     <MDBox mb={3}>
-                    <ProfileInfoCard
-                        title="Contact Info"
-                        info={{
-                        personalEmail: "lexi.thompson@gmail.com",
-                        workEmail: "athompson@companyName.com",
-                        cellPhone: "(111) 111 - 1111",
-                        workPhone: "(222) 222 - 2222",
-                        }}
-                        social={[
-                            {
-                                link: "https://www.wechat.com/",
-                                icon: <ForumIcon />,
-                                color: "github",
-                            },
-                          ]}
-                        action={{ route: "", tooltip: "Edit Profile" }}
-                    />
+                        <Typography variant="h6" gutterBottom sx={{ marginTop: '50px', borderRadius: '10px', backgroundColor: '#6667AB', color: '#FFFFFF', textAlign: 'center', pt: '2px', pb: '2px' }}>
+                            Contact Section
+                        </Typography>
+                        <ProfileInfoCard
+                            title="Contact Information"
+                            info={{
+                            personalEmail: "lexi.thompson@gmail.com",
+                            workEmail: "athompson@companyName.com",
+                            cellPhone: "(111) 111 - 1111",
+                            workPhone: "(222) 222 - 2222",
+                            }}
+                            social={[
+                                {
+                                    link: "https://www.wechat.com/",
+                                    icon: <ForumIcon />,
+                                    color: "github",
+                                },
+                            ]}
+                            action={{ route: "", tooltip: "Edit Profile" }}
+                        />
                     </MDBox>
                 </Grid>
             </Grid>
--- frontend/src/layouts/personalinfo/components/Profile/index.js
+++ frontend/src/layouts/personalinfo/components/Profile/index.js
@@ -19,6 +19,7 @@ import ProfileInfoCard from "examples/Cards/InfoCards/ProfileInfoCard";
 
 // @mui material components
 import Grid from "@mui/material/Grid";
+import Typography from '@mui/material/Typography';
 
 // @mui icons
 import FacebookIcon from '@mui/icons-material/Facebook';
@@ -31,16 +32,19 @@ function Address() {
 
     <MDBox>
       <Grid container spacing={3}>
-        <Grid item xs={12} lg={12}>
+        <Grid item xs={12} md={12} lg={12}>
+        <Typography variant="h6" gutterBottom sx={{ backgroundColor: '#6667AB', borderRadius: '10px', color: '#FFFFFF', textAlign: 'center', pt: '2px', pb: '2px' }}>
+          User Profile Section
+        </Typography>
           <ProfileInfoCard
-            title="User Profile"
+            title="User Profile Information"
             info={{
-              fullName: "Natasha Lee",
-              preferredName: "Natasha",
+              fullName: "Natasha M. Lee",
+              preferredName: "Tasha",
               birthDate: "05 June 1996",
               age: "26",
               gender: "Female",
-              SSN: "***-***-1234",
+              SSN: "123-123-1234", // this should be encrypted 
             }}
             social={[
               {
@@ -62,6 +66,7 @@ function Address() {
             action={{ route: "", tooltip: "Edit Profile" }}
           />
         </Grid>
+
         {/* <Grid item xs={12} lg={6}>
           <ProfileInfoCard
             title="Secondary Address"
