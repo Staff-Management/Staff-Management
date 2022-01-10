9b0c4339b804af679b31f9e09dc5ee5b36d326a5
Erika Yi <yi.erika2@gmail.com>
2022-01-09 22:25:18 -0500

    implemented frontend for the personal dashboard page

--- frontend/src/layouts/personalinfo/components/Address/index.js
+++ frontend/src/layouts/personalinfo/components/Address/index.js
@@ -13,18 +13,42 @@ Coded by www.creative-tim.com
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 */
 
+import React, { useState } from 'react';
+
 // Material Dashboard 2 React components
 import MDBox from "components/MDBox";
-import ProfileInfoCard from "examples/Cards/InfoCards/ProfileInfoCard";
 
 // @mui material components
 import Grid from "@mui/material/Grid";
 import Typography from '@mui/material/Typography';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import TextField from '@mui/material/TextField';
+import { Button, CardActionArea, CardActions, CardHeader } from '@mui/material';
+import { makeStyles } from "@material-ui/core/styles";
 
-// @mui icons
-import LocationOnIcon from '@mui/icons-material/LocationOn';
+const useStyles = makeStyles({
+  root: {
+    minWidth: 250
+  },
+  bullet: {
+    display: "flex",
+    margin: "0 2px",
+    transform: "scale(0.8)"
+  },
+  title: {
+    fontSize: 14
+  },
+  pos: {
+    marginBottom: 12
+  }
+});
 
 function Address() {
+  
+  const classes = useStyles();
+  const [primaryupdating, setPrimaryUpdating] = useState(false);
+  const [secondaryupdating, setSecondaryUpdating] = useState(false);
 
   return (
 
@@ -36,28 +60,203 @@ function Address() {
 
       <Grid container spacing={3}>
         <Grid item xs={12} lg={6}>
-          <ProfileInfoCard
-            title="Primary Address Information"
-            info={{
-              addressLine1: "1111 Street Name",
-              addressLine2: "Apt #123",
-              city: "Philadelphia",
-              state: "PA",
-              zip: "07123",
-            }}
-            social={[
-              {
-                link: "https://www.google.com/maps",
-                icon: <LocationOnIcon />,
-                color: "github",
-              },
-            ]}
-            action={{ route: "", tooltip: "Edit Profile" }}
-          />
+          <Card className={classes.root} variant="outlined" sx={{ maxWidth: 500 }}>
+            <CardActionArea>
+              <CardContent>
+              {primaryupdating ? (
+                <div>
+                  <div>
+                    <TextField
+                      label="Update Address Line 1"
+                      id="outlined-size-small"
+                      defaultValue="Address Line 1"
+                      size="small"
+                    />
+                  </div>
+                  <br />
+                  <div>
+                    <TextField
+                      label="Update Address Line 2"
+                      id="outlined-size-small"
+                      defaultValue="Address Line 2"
+                      size="small"
+                    />
+                  </div>
+                  <br />
+                  <div>
+                    <TextField
+                      label="Update City"
+                      id="outlined-size-small"
+                      defaultValue="City"
+                      size="small"
+                    />
+                  </div>
+                  <br />
+                  <div>
+                    <TextField
+                      label="Update State"
+                      id="outlined-size-small"
+                      defaultValue="State"
+                      size="small"
+                    />
+                  </div>
+                  <br />
+                  <div>
+                    <TextField
+                      label="Update Zip Code"
+                      id="outlined-size-small"
+                      defaultValue="Zip Code"
+                      size="small"
+                    />
+                  </div>
+                </div>                
+              ) : (
+                <div>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Primary Address:
+                  </Typography>
+
+                  Address Line 1:
+                  <Typography gutterBottom variant="h6" component="div">
+                    1111 Street Name
+                  </Typography>
+
+                  Address Line 2:
+                  <Typography gutterBottom variant="h6" component="div">
+                    Apt #123
+                  </Typography>
+
+                  City:
+                  <Typography gutterBottom variant="h6" component="div">
+                    Philadelphia
+                  </Typography>
+
+                  State:
+                  <Typography gutterBottom variant="h6" component="div">
+                    PA
+                  </Typography>
+
+                  Zip Code:
+                  <Typography gutterBottom variant="h6" component="div">
+                    07123
+                  </Typography>
+                </div>
+              )}  
+              </CardContent>
+            </CardActionArea>
+
+            <CardActions>
+              <Button size="small" onClick={() => setPrimaryUpdating(true)} color="secondary">
+                Update
+              </Button>
+              <Button size="small" onClick={() => setPrimaryUpdating(false)} color="secondary">
+                Back
+              </Button>
+            </CardActions>
+
+          </Card>
+
         </Grid>
 
         <Grid item xs={12} lg={6}>
-          <ProfileInfoCard
+          <Card className={classes.root} variant="outlined" sx={{ maxWidth: 500 }}>
+              <CardActionArea>
+                <CardContent>
+                {secondaryupdating ? (
+                  <div>
+                    <div>
+                      <TextField
+                        label="Update Address Line 1"
+                        id="outlined-size-small"
+                        defaultValue="Address Line 1"
+                        size="small"
+                      />
+                    </div>
+                    <br />
+                    <div>
+                      <TextField
+                        label="Update Address Line 2"
+                        id="outlined-size-small"
+                        defaultValue="Address Line 2"
+                        size="small"
+                      />
+                    </div>
+                    <br />
+                    <div>
+                      <TextField
+                        label="Update City"
+                        id="outlined-size-small"
+                        defaultValue="City"
+                        size="small"
+                      />
+                    </div>
+                    <br />
+                    <div>
+                      <TextField
+                        label="Update State"
+                        id="outlined-size-small"
+                        defaultValue="State"
+                        size="small"
+                      />
+                    </div>
+                    <br />
+                    <div>
+                      <TextField
+                        label="Update Zip Code"
+                        id="outlined-size-small"
+                        defaultValue="Zip Code"
+                        size="small"
+                      />
+                    </div>
+                  </div>                
+                ) : (
+                  <div>
+                    <Typography gutterBottom variant="h5" component="div">
+                      Secondary Address:
+                    </Typography>
+
+                    Address Line 1:
+                    <Typography gutterBottom variant="h6" component="div">
+                      1234 Street Name
+                    </Typography>
+
+                    Address Line 2:
+                    <Typography gutterBottom variant="h6" component="div">
+                      Apt #123
+                    </Typography>
+
+                    City:
+                    <Typography gutterBottom variant="h6" component="div">
+                      East Windsor
+                    </Typography>
+
+                    State:
+                    <Typography gutterBottom variant="h6" component="div">
+                      NJ
+                    </Typography>
+
+                    Zip Code:
+                    <Typography gutterBottom variant="h6" component="div">
+                      08123
+                    </Typography>
+                  </div>
+                )}
+                </CardContent>
+              
+              </CardActionArea>
+
+              <CardActions>
+                <Button size="small" onClick={() => setSecondaryUpdating(true)} color="secondary">
+                  Update
+                </Button>
+                <Button size="small" onClick={() => setSecondaryUpdating(false)} color="secondary">
+                  Back
+                </Button>
+              </CardActions>
+            
+            </Card>
+            
+          {/* <ProfileInfoCard
             title="Secondary Address Information"
             info={{
               addressLine1: "1111 Street Name",
@@ -74,7 +273,8 @@ function Address() {
               },
             ]}
             action={{ route: "", tooltip: "Edit Profile" }}
-          />
+          /> */}
+
         </Grid>
 
       </Grid>
